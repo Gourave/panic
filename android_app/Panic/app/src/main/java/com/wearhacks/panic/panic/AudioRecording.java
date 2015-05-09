375b755880b43a9164b88a10dd14b874e85a96cd
Goverm <gouravev@gmail.com>
2015-05-09 04:17:48 -0400

    audio recording class setup

--- android_app/Panic/app/src/main/java/com/wearhacks/panic/panic/AudioRecording.java
+++ android_app/Panic/app/src/main/java/com/wearhacks/panic/panic/AudioRecording.java
@@ -1,6 +1,10 @@
 package com.wearhacks.panic.panic;
 
 import android.media.MediaRecorder;
+import android.os.Debug;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class AudioRecording {
 
@@ -10,19 +14,36 @@ public class AudioRecording {
         mRecorder = null;
     }
 
-    private void onRecord(boolean start) {
-        if (start)
-            startRecording();
+    private MediaRecorder onRecord(boolean start) {
+        if (start) {
+            return startRecording();
+        }
         else
-            stopRecording();
+            return stopRecording();
     }
 
-    private void startRecording() {
+    private MediaRecorder startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        mRecorder.start();
+
+        return null;
     }
 
-    private void stopRecording() {
-        
+    private MediaRecorder stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+
+        return mRecorder;
     }
 
 }
