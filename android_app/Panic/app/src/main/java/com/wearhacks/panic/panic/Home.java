4b9751943b2cfacb67c3857cb9eb1f182af90cbf
Goverm <gouravev@gmail.com>
2015-05-09 01:12:54 -0400

    Added Settings Page

--- android_app/Panic/app/src/main/java/com/wearhacks/panic/panic/Home.java
+++ android_app/Panic/app/src/main/java/com/wearhacks/panic/panic/Home.java
@@ -1,5 +1,6 @@
 package com.wearhacks.panic.panic;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -42,6 +43,9 @@ public class Home extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent mOpenSettings = new Intent(this, Settings.class);
+            startActivity(mOpenSettings);
+
             return true;
         }
 
