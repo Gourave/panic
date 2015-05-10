7a284d3a89bb8975d9c6a3a9a8154c448a5e9e43
Navjot Panesar <navjotspanesar@gmail.com>
2015-05-10 08:20:07 -0400

    Enabled cross domain access

--- api/app.js
+++ api/app.js
@@ -27,6 +27,16 @@ app.use(multer({
         return filename;
     }
 }));
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
